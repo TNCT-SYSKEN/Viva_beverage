b8417e24702c69ad04e456f6a1d690ca156e42eb
villach927 <villach927@gmail.com>
2017-02-04 17:23:25 +0900

    update views

--- app/controllers/vendings_controller.rb
+++ app/controllers/vendings_controller.rb
@@ -6,29 +6,37 @@ class VendingsController < ApplicationController
     n_vendings = []
 
     @vendings = []
+    @drinks = []
 
     if !user_params["genre"].blank? && !user_params["name"].blank?
 
-      drinks = Drink.where("name like '%" + user_params["name"] + "%' and genre_id =" + user_params["genre"])
-      drinks.map{ |d| d.ve_relation_ship.map{ |r| @vendings.push(Vending.where(id: r.vending_id)) } }
+      drinks_t = Drink.where("name like '%" + user_params["name"] + "%' and genre_id =" + user_params["genre"])
+      drinks_t.map{ |d| d.ve_relation_ship.map{ |r| @vendings.push(Vending.where(id: r.vending_id)) } }
+
+      @drinks.push(drinks)
 
     elsif !user_params["genre"].blank?
 
-      drinks = Drink.where(genre_id: user_params["genre"])
-      drinks.map{ |d| d.ve_relation_ship.map{ |r| @vendings.push(Vending.where(id: r.vending_id)) } }
+      drinks_t = Drink.where(genre_id: user_params["genre"])
+      drinks_t.map{ |d| d.ve_relation_ship.map{ |r| @vendings.push(Vending.where(id: r.vending_id)) } }
+
+      @drinks.push(drinks)
 
     elsif !user_params["name"].blank?
 
-      drinks = Drink.where("name like '%" + user_params["name"] + "%'")
-      drinks.map{ |d| d.ve_relation_ship.map{ |r| @vendings.push(Vending.where(id: r.vending_id)) } }
+      drinks_t = Drink.where("name like '%" + user_params["name"] + "%'")
+      drinks_t.map{ |d| d.ve_relation_ship.map{ |r| @vendings.push(Vending.where(id: r.vending_id)) } }
+
+      @drinks.push(drinks)
 
     else
+
       puts "hoge"
       @vendings.push(Vending.all)
     end
 
     @vendings = @vendings.flatten
-
+    @drinks = @drinks.flatten
   end
 
   private
