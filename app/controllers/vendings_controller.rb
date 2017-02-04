46bdae67326b22a8d73cedf41b148fa005190218
villach927 <villach927@gmail.com>
2017-02-04 14:47:02 +0900

    update vendings controller

--- app/controllers/vendings_controller.rb
+++ app/controllers/vendings_controller.rb
@@ -1,4 +1,18 @@
 class VendingsController < ApplicationController
   def show
+    @vendings = Vending.all
+    drinks = Drink.all
+    if user_params.include?("genre")
+      drinks = drinks.select{|d| d.genre_id == user_params["genre"]}
+
+      @vendings = @vendings.select{ |v|
+         
+      }
+    end
   end
+
+  private
+    def user_params
+      params
+    end
 end
