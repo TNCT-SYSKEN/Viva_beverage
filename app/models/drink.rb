80488fb145a869f8d91d3637662c93828383926e
mainasu273 <exapmle@example.com>
2017-02-04 14:39:38 +0900

    model hosmany and belong

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,2 +1,4 @@
 class Drink < ApplicationRecord
+  has_many :ve_relation_ship
+  has_many :vending, through: :ve_relation_ship 
 end
--- app/models/ve_relation_ship.rb
+++ app/models/ve_relation_ship.rb
@@ -1,2 +1,4 @@
 class VeRelationShip < ApplicationRecord
+  belongs_to :vending
+  belongs_to :drink
 end
