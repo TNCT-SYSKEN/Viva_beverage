030edc30d103f3e7dff710949749c1c2e9ecb392
villach927 <villach927@gmail.com>
2017-02-04 15:25:30 +0900

    update migrate

--- db/migrate/20170204041503_create_ve_relation_ships.rb
+++ db/migrate/20170204041503_create_ve_relation_ships.rb
@@ -1,8 +1,8 @@
 class CreateVeRelationShips < ActiveRecord::Migration[5.0]
   def change
     create_table :ve_relation_ships do |t|
-      t.integer :ven_id
-      t.integer :dri_id
+      t.integer :vending_id
+      t.integer :drink_id
       t.integer :temp
       t.integer :value
 
