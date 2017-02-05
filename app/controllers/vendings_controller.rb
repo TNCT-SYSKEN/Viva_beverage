6c8353b6d85290d79ecff069df569750cbc9abdf
villach927 <villach927@gmail.com>
2017-02-05 11:42:34 +0900

    ほげ

--- app/controllers/vendings_controller.rb
+++ app/controllers/vendings_controller.rb
@@ -16,23 +16,96 @@ class VendingsController < ApplicationController
     if !user_params["genre"].blank? && !user_params["name"].blank?
 
       drinks_t = Drink.where("name like '%" + user_params["name"] + "%' and genre_id =" + user_params["genre"])
-      drinks_t.map{ |d| d.ve_relation_ship.map{ |r| @vendings.push(Vending.where(id: r.vending_id));@relations[r.vending_id].push(d.name) } }
+      if user_params["temp"].blank?
+        drinks_t.map{ |d| d.ve_relation_ship.map{ |r| @vendings.push(Vending.where(id: r.vending_id));@relations[r.vending_id].push(d.name) } }
+      else
+        drinks_t.map{ |d|
+          d.ve_relation_ship.map{ |r|
+            if r.temp == user_params["temp"].to_i
+              @vendings.push(Vending.where(id: r.vending_id))
+              @relations[r.vending_id].push(d.name)
+              @drinks.push(Drink.where(id: r.drink_id))
+            end
+          }
+        }
+      end
 
-      @drinks.push(drinks_t)
+      drinks_t.map{ |d|
+        if foo = user_params["temp"].blank?
+          @drinks.push(d)
+        else
+          hoge = false
+
+          d.ve_relation_ship.map{|r|
+            hoge = true if r.temp == user_params["temp"].to_i
+          }
+
+          @drinks.push(d) if hoge == true
+        end
+      }
 
     elsif !user_params["genre"].blank?
 
       drinks_t = Drink.where(genre_id: user_params["genre"])
-      drinks_t.map{ |d| d.ve_relation_ship.map{ |r| @vendings.push(Vending.where(id: r.vending_id));@relations[r.vending_id].push(d.name) } }
 
-      @drinks.push(drinks_t)
+      if user_params["temp"].blank?
+        drinks_t.map{ |d| d.ve_relation_ship.map{ |r| @vendings.push(Vending.where(id: r.vending_id));@relations[r.vending_id].push(d.name) } }
+      else
+        drinks_t.map{ |d|
+          d.ve_relation_ship.map{ |r|
+            if r.temp == user_params["temp"].to_i
+              @vendings.push(Vending.where(id: r.vending_id))
+              @relations[r.vending_id].push(d.name)
+              @drinks.push(Drink.where(id: r.drink_id))
+            end
+          }
+        }
+      end
+
+      drinks_t.map{ |d|
+        if foo = user_params["temp"].blank?
+          @drinks.push(d)
+        else
+          hoge = false
+
+          d.ve_relation_ship.map{|r|
+            hoge = true if r.temp == user_params["temp"].to_i
+          }
+
+          @drinks.push(d) if hoge == true
+        end
+      }
 
     elsif !user_params["name"].blank?
 
       drinks_t = Drink.where("name like '%" + user_params["name"] + "%'")
-      drinks_t.map{ |d| d.ve_relation_ship.map{ |r| @vendings.push(Vending.where(id: r.vending_id));@relations[r.vending_id].push(d.name) } }
+      if user_params["temp"].blank?
+        drinks_t.map{ |d| d.ve_relation_ship.map{ |r| @vendings.push(Vending.where(id: r.vending_id));@relations[r.vending_id].push(d.name) } }
+      else
+        drinks_t.map{ |d|
+          d.ve_relation_ship.map{ |r|
+            if r.temp == user_params["temp"].to_i
+              @vendings.push(Vending.where(id: r.vending_id))
+              @relations[r.vending_id].push(d.name)
+              @drinks.push(Drink.where(id: r.drink_id))
+            end
+          }
+        }
+      end
 
-      @drinks.push(drinks_t)
+      drinks_t.map{ |d|
+        if foo = user_params["temp"].blank?
+          @drinks.push(d)
+        else
+          hoge = false
+
+          d.ve_relation_ship.map{|r|
+            hoge = true if r.temp == user_params["temp"].to_i
+          }
+
+          @drinks.push(d) if hoge == true
+        end
+      }
 
     else
 
